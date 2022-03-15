85883162706ad1ed170efede37b3ae59010400a2
Melike Aslan <1melike2@gmail.com>
2022-03-16 00:19:08 +0300

    kodluyoruz javascript video serisine başlandi.

---
+++ kodluyoruz_Javascript/js/01-hello-world.js
@@ -0,0 +1,11 @@
+/*document.write('merhaba dünya');
+document.write('merhaba dünya');
+
+alert("alert ile merhaba dünya");
+
+alert("alert ile merhaba dünya 22")
+
+alert('alert ile merhaba dünya ikinci deneme')
+
+console.log("console log ile merhaba dunya")
+console.log("console log ile merhaba dunya 22")    */
---
+++ kodluyoruz_Javascript/js/02-degisken-tanimlama.js
@@ -0,0 +1,9 @@
+var serverName="kodluyoruz.js"
+console.log(serverName)
+
+document.write(serverName)
+document.write("hey")
+
+document.write(serverName2)
+let serverName2="https,kodluyorüz"    //var yapsaydık undefined derdi.
+
